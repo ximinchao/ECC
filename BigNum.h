bfda0c1f099d73e910498d00fd04749fd813a898
ximinchao <ximinchao@163.com>
2014-03-25 16:04:40 +0800

    temporary commit

--- BigNum.h
+++ BigNum.h
@@ -3,8 +3,30 @@
 
 #include "ECCType.h"
 
+struct BNListNode
+{
+	u8 m_ucData;
+	BNListNode *prev, *next;
+};
+
+class BNList
+{
+private:
+	BNListNode *m_head, *m_end;
+	u32 m_size;
+public:
+	BNList();
+	~BNList();
+
+	u32 getSize();
+	bool addHead(u8 ucData);
+	bool addEnd(u8 ucData);
+	
+}
+
 class BigNum
 {
+private:
 public:
 	BigNum();
 	BigNum(const BigNum& bn);
